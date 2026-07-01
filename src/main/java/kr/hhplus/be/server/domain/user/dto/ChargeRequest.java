00fd984c3e95872315d512300735e1cf74cd0ffd
ChoiSW <seungwoo714@gmail.com>
2026-07-02 03:13:30 +0900

    [FEAT] User/Wallet 도메인 DTO 생성
    PointResponse, ChargeRequest, ChargeResponse

---
+++ src/main/java/kr/hhplus/be/server/domain/user/dto/ChargeRequest.java
@@ -0,0 +1,10 @@
+package kr.hhplus.be.server.domain.user.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ChargeRequest {
+    private Long amount;
+}
