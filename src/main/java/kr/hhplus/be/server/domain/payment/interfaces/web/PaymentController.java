1f7bc1cc6dacb63ea8e57910dbd6a9cb1d79f203
ChoiSW99 <seungwoo714@gmail.com>
2026-07-04 15:34:08 +0900

    [FEAT] ReservationController 좌석 임시 배정 및 PaymentController 결제 엔드포인트 구현

--- src/main/java/kr/hhplus/be/server/domain/payment/interfaces/web/PaymentController.java
+++ src/main/java/kr/hhplus/be/server/domain/payment/interfaces/web/PaymentController.java
@@ -1,7 +1,34 @@
 package kr.hhplus.be.server.domain.payment.interfaces.web;
 
+import kr.hhplus.be.server.domain.payment.application.PaymentService;
+import kr.hhplus.be.server.domain.payment.interfaces.web.dto.PaymentRequest;
+import kr.hhplus.be.server.domain.payment.interfaces.web.dto.PaymentResponse;
+import kr.hhplus.be.server.global.response.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    // 결제요청
+    @PostMapping("/reservations/{reservationId}/payments")
+    public ResponseEntity<ApiResponse<PaymentResponse>> processPayment(
+        @PathVariable Long reservationId,
+        @RequestBody PaymentRequest request
+    ) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(
+                ApiResponse.success(
+                    paymentService.processPayment(reservationId, request.getAmount())
+                )
+            );
+    }
 }
